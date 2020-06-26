714df68f1cb049095ff1ccc6afa6de2f385f1a9c
jokin <327928971@qq.com>
2020-06-26 15:10:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# php-file-browser
+
+基于PHP开发的可生成静态页面的在线文件浏览器
+
+## 使用方法
+
+- 修改`.config`配置
+- 增加文件至根目录
+- 提交`GitHub`
+- 等待`Action`提交`gh-pages`分支
+- 开启`Pages`
+
+## 国内访问加速
+
+- 登录`vercel.com`
+- 导入项目
+- 绑定域名
+- 将该域名设置分支为`gh-pages`
+- 再次触发Deploy即可生效
---
+++ tool.php
@@ -0,0 +1,77 @@
+<?php
+// 读取配置文件
+$config = parse_ini_file('./.config');
+/**
+ * 根目录排开文件
+ * @param  array except
+ */
+$except = [];
+if (isset($config['except'])) {
+  $tmp = $config['except'];
+  $tmp = explode(',', $tmp);
+  foreach ($tmp as $key => $value) {
+    $except[] = trim($value);
+  }
+}
+
+// 扫描目录
+$dirs = scan('.', $except);
+
+// 转换JSON
+$dirs = json_encode($dirs);
+
+// 保存map.json
+file_put_contents('./assets/map.json', $dirs);
+
+// 读取模板
+$tpl = file_get_contents('./assets/index.tpl');
+
+// 标题设置
+if (isset($config['title'])) {
+  $tpl = str_replace('{{__TITLE__}}', $config['title'], $tpl);
+}
+if (isset($config['subtitle_link'])) {
+  $tpl = str_replace('{{__SUBTITLE_LINK__}}', $config['subtitle_link'], $tpl);
+}
+if (isset($config['subtitle_text'])) {
+  $tpl = str_replace('{{__SUBTITLE_TEXT__}}', $config['subtitle_text'], $tpl);
+}
+
+// 关于与鸣谢
+if (isset($config['akm_link'])) {
+  $tpl = str_replace('{{__AKM_LINK__}}', $config['akm_link'], $tpl);
+}
+if (isset($config['akm_text'])) {
+  $tpl = str_replace('{{__AKM_TEXT__}}', $config['akm_text'], $tpl);
+}
+
+// 生成静态文件
+file_put_contents('./index.html', $tpl);
+
+/**
+ * 目录遍历
+ * @param  string path        不含结尾'/'
+ * @param  array  except
+ * @param  bool   recurse     是否在递归中
+ */
+function scan($path, $except = [], $recurse = false) {
+  $_dirs = scandir($path);
+  $dirs = [];
+  foreach ($_dirs as $key => $value) {
+    if ($value == '.' || $value == '..' || $value == '.git') {
+      // 基础排除项
+      continue;
+    }else if ($recurse === false && in_array($value, $except)) {
+      // 额外排除
+      continue;
+    }else if (!is_dir($path.'/'.$value)) {
+      // 非目录不再遍历
+      $dirs[] = $value;
+    }else{
+      // 目录再次遍历
+      $dirs[$value] = scan($path.'/'.$value, [], true);
+    }
+  }
+  return $dirs;
+}
+?>
